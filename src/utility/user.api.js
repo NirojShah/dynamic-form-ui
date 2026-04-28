ef9176c3fdfa9359caadbff49d6c02a38ae453f0
Niraj Shah <nirajshah950@gmail.com>
2026-04-28 23:22:15 +0530

    FEATURE -
    1. add the signup function to handle the user signup.
    
    2. getMe - to check if the user has correct credential or not.

--- src/utility/user.api.js
+++ src/utility/user.api.js
@@ -4,16 +4,48 @@ const login = async ({ email, password }) => {
   try {
     const resp = await methods.post("/user/login", { email, password }, false);
     if (resp.status == "success") {
-      localStorage.setItem()
-      return resp.token;
+      localStorage.setItem("token", resp.token);
+      return {
+        success: true,
+      };
     }
   } catch (err) {
     console.log(err);
   }
 };
 
+const signUp = async ({ name, email, password, organizationId }) => {
+  try {
+    const resp = methods.post("/user/signup", {
+      name,
+      email,
+      password,
+      organizationId,
+    });
+    if (resp.data.status == "success") {
+      console.log("signup successfully.");
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const getMe = async () => {
+  try {
+    const resp = await methods.get("/user/me");
+    return resp;
+  } catch (err) {
+    return {
+      success: false,
+      message: err.message,
+    };
+  }
+};
+
 const userApis = {
   login,
+  signUp,
+  getMe,
 };
 
 export default userApis;
