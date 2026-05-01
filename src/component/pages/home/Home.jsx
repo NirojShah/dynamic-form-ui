ff734d826034346bca7fba38b914c96ab8ebc430
Niraj Shah <nirajshah950@gmail.com>
2026-05-01 22:01:06 +0530

    Conditonal Rendering.

--- src/component/pages/home/Home.jsx
+++ src/component/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import { Outlet, useLocation } from "react-router-dom";
 import Nav from "../../navigation/Nav";
 import VertNav from "../../navigation/VertNav";
 import FieldLibrary from "../../form/FieldLibrary";
+import FieldLayout from "../../form/FieldLayout";
 
 const MainLayout = () => {
   const location = useLocation();
@@ -16,7 +17,6 @@ const MainLayout = () => {
 
       {/* MAIN CONTENT */}
       <div className="flex flex-1 overflow-hidden">
-        
         {/* LEFT SIDE */}
         <div className="border-r border-black/10 w-16 md:w-64">
           {isCreatePage ? <FieldLibrary /> : <VertNav />}
@@ -25,6 +25,10 @@ const MainLayout = () => {
         {/* RIGHT CONTENT */}
         <div className="flex-1 overflow-y-auto p-6">
           <Outlet />
+        </div>
+
+        <div>
+          <FieldLayout />
         </div>
       </div>
     </div>
