9ab3f523651a3c066d6fd17ef35b562986249be2
Niraj Shah <nirajshah950@gmail.com>
2026-05-07 17:19:37 +0530

    An component to display the Forms response.

--- src/component/pages/form response/FormResponse.jsx
+++ src/component/pages/form response/FormResponse.jsx
@@ -2,24 +2,30 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import formsApi from '../../../utility/forms.api';
 import BackButton from '../../common/BackButton';
+import DynamicResponseTable from '../../common/DynamicResponseTable';
 
 const FormResponse = () => {
     const { formId } = useParams()
     const [response, setResponse] = useState([]);
+    const [fields, setFields] = useState([]);
+    const [page, setPage] = useState(1);
+    const [limit, setLimit] = useState(10);
 
     useEffect(() => {
+        // setPage(1);
+        // setLimit(10);
         const fetchResponse = async (formId) => {
-            const resp = await formsApi.getResponses(formId)
-            setResponse(resp.data)
+            const resp = await formsApi.getResponses({ key: formId, page, limit })
+            setResponse(resp.data);
+            setFields(resp.fields);
         }
-
         fetchResponse(formId);
-    }, [])
+    }, [formId])
 
     return (
-        <div className='flex flex-row'>
+        <div className='flex flex-col items-start gap-2'>
             <BackButton title={"My form"} />
-            
+            <DynamicResponseTable fields={fields} data={response} />
         </div>
     )
 }
