a5d0b0c4ac3f5771d228f18988782798351b4295
Niraj Shah <nirajshah950@gmail.com>
2026-05-20 16:59:46 +0530

    UPDATE - disabled the check for es-lint.

--- src/component/pages/form/templates/Template.jsx
+++ src/component/pages/form/templates/Template.jsx
@@ -45,6 +45,7 @@ const Template = () => {
 
     useEffect(() => {
         fetchPublicForms();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [page, limit]);
 
     const handleClick = (id) => {
